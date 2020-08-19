0b82f659154cd5662153717f2191e5fc84cd1699
Carlos Salinas <salinas.carlos@uabc.edu.mx>
2020-08-19 16:49:53 -0700

    Version 2.0

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.android.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var inputMethodManager: InputMethodManager? = null
+    var editText: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editText = findViewById(R.id.nickname_edit)
+        inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        findViewById<Button>(R.id.done_button).setOnClickListener{addNickname(it)}
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener { updateNickname(it) }
+    }
+
+    private fun addNickname (view: View) {
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        nicknameTextView.text = editText?.text
+        editText?.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+        inputMethodManager?.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickname (view: View) {
+        val doneButton = findViewById<Button>(R.id.done_button)
+        editText?.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+        editText?.requestFocus()
+        inputMethodManager?.showSoftInput(editText, 0)
     }
 }
